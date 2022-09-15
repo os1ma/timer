77925573dd5904c1893519979fd64975454dabf0
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2022-09-15 23:54:56 +0900

    Validate args[1]

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,19 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    if args.len() <= 2 {
+        println!("No command specified");
+        return;
+    }
+
     let command = &args[1];
     let options: Vec<String> = args[2..args.len()].to_vec();
 
     println!("command = {}", command);
     println!("options = {:?}", options);
 
-    timer::timer();
+    match command.as_str() {
+        "timer" => timer::timer(),
+        _ => println!("Unsupported command '{}'", command),
+    }
 }
