75258f10071ebcc1c11816b0776d3507228b80a4
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2022-09-15 23:48:40 +0900

    Parse arguments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 mod timer;
 
+use std::env;
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let command = &args[1];
+    let options: Vec<String> = args[2..args.len()].to_vec();
+
+    println!("command = {}", command);
+    println!("options = {:?}", options);
+
     timer::timer();
 }
